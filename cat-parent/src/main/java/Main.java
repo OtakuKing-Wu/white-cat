5d9fc5680581597d554cca7105a15b3a8a2148ec
武堃 <wukun07@meituan.com>
2019-05-01 23:34:41 +0800

    init test configuration

---
+++ cat-parent/src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Description： test main class
+ * Author: wukun
+ * Date: Created in 2019/5/1 下午11:30
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--just test--");
+    }
+}
